8bb4eb92ec292bb7fbec556cbb967a365d1893b7
pmbisah <petermbisah@gmail.com>
2025-03-28 16:15:23 +0200

    Add PriceCompareApp/metro.config.js 1743171321893

---
+++ PriceCompareApp/metro.config.js
@@ -0,0 +1,19 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json']
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
